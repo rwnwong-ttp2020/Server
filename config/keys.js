5dadd575e26da3126cb1699bb4ffc2fcd7d3afa1
rwnwong <rwnwong@gmail.com>
2020-12-30 18:50:48 -0500

    trying to fix scr error

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,10 @@
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
     //we are in production - return the prod set of keys
+    modules.exports = require('./prod');
 } 
 else {
-    //we are in development - return the dev keys
+    //we are in development - return the dev
+    module.exports = require ('/dev');
+
 }
